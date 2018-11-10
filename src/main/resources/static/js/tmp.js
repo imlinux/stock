f315be762273db90fae46cbf1627da9e5c00d752
董帅阳 <7518925@gmail.com>
2018-11-10 13:05:41 +0800

    使用npm管理前端

---
+++ src/main/resources/static/js/tmp.js
@@ -0,0 +1,13 @@
+
+
+
+var a = 100;
+
+var c = 2000;
+
+function f() {
+    console.log("hello")
+}
+export default {
+    name:a
+}
---
+++ src/main/resources/static/webpack.config.js
@@ -0,0 +1,45 @@
+
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ],
+  resolve:{
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  }
+}
