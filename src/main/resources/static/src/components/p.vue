4dfbd5792fc001c714f8b48f7068ba4e84739f38
董帅阳 <7518925@gmail.com>
2018-11-14 23:56:07 +0800

    添加测试代码

---
+++ src/main/resources/static/src/components/p.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <c v-bind:msg="message"></c>
+    <input type="button" v-on:click="click"/>
+    </div>
+</template>
+
+<script>
+    import c from './c'
+
+    export default {
+        components: {
+            c
+        },
+        data: function () {
+            return {
+                "message":"hello"
+            }
+        },
+        methods:{
+            click:function () {
+                this.message = "AAAA";
+            }
+        }
+    }
+</script>
--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -1,9 +1,11 @@
-import App from './components/app.vue'
+import App from './components/app'
+import P from './components/p'
 
 import Vue from 'vue'
 
 const vm = new Vue({
     el:'#app',
+
     render:function (h) {
         return h(App);
     }
